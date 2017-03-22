5514419c408ab0cb7cc402a5b35d8b85aa3d6119
Tyna Huynh <tynahuynh@gmail.com>
2017-03-22 05:59:41 +0000

    added lessons

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :courses, only:[:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [:new, :create] 
     end
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
