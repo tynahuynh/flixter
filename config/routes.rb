400af8bad422b9a1a059f736590976379a73d507
Tyna Huynh <tynahuynh@gmail.com>
2017-03-23 04:49:53 +0000

    finalizing permissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only:[:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create] 
